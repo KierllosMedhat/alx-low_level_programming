4f47406ce5795e25edd34c9636167ed1a7f2c812
Kierllos <KierllosMedhat@outlook.com>
2023-03-20 18:19:02 -0700

    shot2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints some words
+ * Return: 0 for succes
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
