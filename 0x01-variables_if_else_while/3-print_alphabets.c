8a9de8bbdac0ad35021f200391bb8c6c0aa38295
Kierllos <KierllosMedhat@outlook.com>
2023-03-21 23:18:48 +0200

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 	char alphabet[54] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 54; i++)
+	for (i = 0; i < 53; i++)
 	{
 		putchar(alphabet[i]);
 	}
