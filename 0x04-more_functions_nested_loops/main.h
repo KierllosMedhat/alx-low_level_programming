fb2b314ba9746c645d9f3a885337875bd0764af3
Kierllos <KierllosMedhat@outlook.com>
2023-03-25 01:35:34 +0200

    print nums

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _isupper - checks for uppercase and lowercase
- * @c: alphabet letter
- * Return: 0 for lowercase and 1 for uppercase
- */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif /* MAIN_H */
