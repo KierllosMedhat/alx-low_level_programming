8d8b70503330f0c5a260489a56ef2f8e0fc4d6b5
Kierllos <KierllosMedhat@outlook.com>
2023-03-24 17:36:42 +0200

    fix 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,17 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <2-largest_number.c>
 /**
  * positive_or_negative - Test for positive and negative numbers
  * @i: the number to be checked
  */
-void positive_or_negative(int i)
-{
-	if (i < 0)
-		printf("%d is negative", i);
-	else
-		printf("%d is positive", i);
-}
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
