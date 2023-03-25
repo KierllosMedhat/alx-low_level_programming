af5e50d91f5fca941921c0b93cf60f9dcb51025f
Kierllos <KierllosMedhat@outlook.com>
2023-03-25 02:04:56 +0200

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * pritn_most_numbers - prints 0 throught 9 without 2 or 4
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' || c == '4')
+			continue;
+		else
+			_putchar(c);
+	}
+	_putchar('\n');
+}
