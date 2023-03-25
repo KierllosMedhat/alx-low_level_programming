7763e3023bae1351a1fa17cf453a3ea643956798
Kierllos <KierllosMedhat@outlook.com>
2023-03-25 18:03:18 +0200

    combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(48 + i);
 		if (i < 9)
 		{
 			putchar(',');
-			putchar(' ');		
+			putchar(' ');
 		}
 
 	}
