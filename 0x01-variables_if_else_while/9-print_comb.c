d80ef3d1ac12dd5297ae26a5473fca0f8641f7c9
Kierllos <KierllosMedhat@outlook.com>
2023-03-25 18:01:33 +0200

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - print comp task
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		putchar(48 + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');		
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
