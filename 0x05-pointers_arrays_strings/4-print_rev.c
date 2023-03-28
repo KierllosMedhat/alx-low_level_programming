9c98dfeb27b95359d67b1bb03974ac259a0bdca2
Kierllos <KierllosMedhat@outlook.com>
2023-03-28 02:54:55 +0200

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,9 @@ void print_rev(char *s)
 	int i, j, len;
 
 	i = 0;
-	while (*(str + i) != '\0')
+	while (*(s + i) != '\0')
 	{
-		_putchar(*(str + i));
+		_putchar(*(s + i));
 		i++;
 	}
 	
@@ -19,7 +19,7 @@ void print_rev(char *s)
 	j = len - 1;
 	while (j >= 0)
 	{
-		_putchar(*(str + j));
+		_putchar(*(s + j));
 		j--;
 	}
 }
