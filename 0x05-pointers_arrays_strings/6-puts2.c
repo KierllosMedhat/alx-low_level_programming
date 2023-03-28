19ee4b3f7ba0f3de0088a0bd2028e30b2b3e2bfc
Kierllos <KierllosMedhat@outlook.com>
2023-03-28 03:35:24 +0200

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,19 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int len, i;
 
-	i = 0;
-	while (*(str + i) != '\0')
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		len++;
 	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
