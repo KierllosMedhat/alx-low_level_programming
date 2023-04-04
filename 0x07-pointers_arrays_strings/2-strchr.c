819ac4fff91302493180d82ccccd821ba4b9cc9f
Kierllos <KierllosMedhat@outlook.com>
2023-04-04 02:34:04 +0200

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 
 
 /**
- * _strchar - locates a character
+ * _strchr - locates a character
  * @s: string
  * @c: the char we r looking for
  * Return: pointer to char or NULL
@@ -10,7 +10,7 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
