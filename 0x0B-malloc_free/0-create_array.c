1ecb5d0cd694f8a4926f1ca273db7bda600ad957
Kierllos <KierllosMedhat@outlook.com>
2023-04-19 14:59:35 +0200

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (NULL);
-	*c_array = malloc(sizeof(char) * size);
+	*c_array = (char *) malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 	{
