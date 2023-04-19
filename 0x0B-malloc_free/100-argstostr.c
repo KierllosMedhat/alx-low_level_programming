5bac5faf3cc908fbf997f0aeff9ddcc320571f53
Kierllos <KierllosMedhat@outlook.com>
2023-04-19 18:14:30 +0200

    task 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - arguments to strings
+ * @ac: args count
+ * @av: args vector
+ * Return: NULL for failure or pointer to string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, strLength;
+	char *string;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			strLength++;
+		strLength++;
+	}
+
+	string = malloc((strlength + 1) * sizeof(char));
+	if (string == NULL)
+	{
+		free(string);
+		return (NULL);
+	}
+
+	for (i = k = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			string[k] = av[i][j];
+			k++;
+		}
+		string[k] = '\n';
+		k++;
+	}
+	return (string);
+
+}
