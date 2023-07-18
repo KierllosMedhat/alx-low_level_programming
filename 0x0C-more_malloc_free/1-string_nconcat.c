4ab1e19a435af32a2cca9eefb4f28c543b43281a
Kierllos <KierllosMedhat@outlook.com>
2023-07-18 04:30:13 +0200

    Task One

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		concatStr[i] = s1[i];
 	for (i = 0; i < n && s2[i]; i++)
-		concatStr[i] = s2[i - (len++)];
+		concatStr[len] = s2[i - (len++)];
 	concatStr[len] = '\0';
 	return (concatStr);
 }
